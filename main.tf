bd32296338934efbb6c476eec8e3594851f491a0
mohiniphatak <phatakmohini@gmail.com>
2024-03-14 12:15:07 +0530

    comment main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "example_terraform_1" {
-  instance_type = var.ec2_instance_type
-  ami           = var.ec2_image
-}
+# resource "aws_instance" "example_terraform_1" {
+#   instance_type = var.ec2_instance_type
+#   ami           = var.ec2_image
+# }
